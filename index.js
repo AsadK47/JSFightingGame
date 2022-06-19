46f8627c1f570ece1a1e820e4adb8a9d13e46726
AsadK47 <asad_khan47@hotmail.co.uk>
2022-06-19 18:27:51 +0100

    Completed move characters and event listeners

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ canvas.height = 576;
 // Create black box
 context.fillRect(0, 0, canvas.width, canvas.height)
 
-const gravity = 0.2
+const gravity = 0.7
 
 class Sprite {
     constructor({ position, velocity }) {
@@ -94,16 +94,16 @@ function animate() {
 
     // Player movement
     if (keys.a.pressed && player.lastKey === 'a') {
-        player.velocity.x = -1
+        player.velocity.x = -5
     } else if (keys.d.pressed && player.lastKey === 'd') {
-        player.velocity.x = 1
+        player.velocity.x = 5
     }
 
     // Enemy movement
     if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
-        enemy.velocity.x = -1
+        enemy.velocity.x = -5
     } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
-        enemy.velocity.x = 1
+        enemy.velocity.x = 5
     }
 }
 
@@ -121,7 +121,7 @@ window.addEventListener('keydown', (event) => {
             player.lastKey = 'a'
             break;
         case 'w':
-            player.velocity.y = -10
+            player.velocity.y = -20
             break;
 
         case 'ArrowRight':
@@ -133,7 +133,7 @@ window.addEventListener('keydown', (event) => {
             enemy.lastKey = 'ArrowLeft'
             break;
         case 'ArrowUp':
-            enemy.velocity.y = -10
+            enemy.velocity.y = -20
             break;
     }
 
